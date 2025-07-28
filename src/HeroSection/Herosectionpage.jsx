047c6e0ba60dd377926be1d5058fc647871764fa
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-07-28 23:34:13 +0100

    'add'

--- src/HeroSection/Herosectionpage.jsx
+++ src/HeroSection/Herosectionpage.jsx
@@ -63,7 +63,7 @@ const Herosectionpage = () => {
 </div>
 
         
-    <div className=' md:h-[55vh] h-[60vh] w-[100vw]  flex flex-col justify-center md:justify-end '>
+    <div className=' md:h-[55vh] h-[60vh] w-[100vw]  flex flex-col pt-10  md:justify-end '>
 
 
 
